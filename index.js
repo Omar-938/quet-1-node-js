1815bcad0449515386351469ef1c5e45315a6b52
BENSADIA Omar (SNCF RESEAU / EIC PARIS NORD / Siège UO NIF) <9505883g@commun.ad.sncf.fr>
2023-04-17 12:28:08 +0200

    ajout du tableau oublier merci Bryce

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const omarInfo = require('./information');
+const obj = require('./information');
 
 
 const cowsay = require("cowsay")
 
 console.log(cowsay.say({
-text : omarInfo.name + " " + omarInfo.campus,
+text : obj.name + " " + obj.campus,
 e : "oO",
 T : " U "
 }));
--- information.js
+++ information.js
@@ -1,8 +1,7 @@
-const name = 'Omar Bensadia';
-const campus = 'Wild Code School';
-
-
-module.exports = {
-	name: name,
-	campus: campus,
+const obj = {
+name: 'Omar Bensadia',
+campus: 'Wild Code School'
 };
+
+
+module.exports = obj
